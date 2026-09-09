12e78970a7d949f3e1c2367c29ff4d301dea01b0
alexherendaln <alexherendaln@gmail.com>
2026-09-09 19:55:31 +0200

    basic attack trigger

--- Assets/Character_script.cs
+++ Assets/Character_script.cs
@@ -1,17 +1,33 @@
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Character_script : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    public BoxCollider2D sword1_prefab;
+    private BoxCollider2D sword1_instance;
+    private bool sword_exists = false;
+    public float sword_lifetime_static;
+    private float sword_lifetime = 0f;
+
+    public float sword_equip = 1;
+
+    private float direction = 1;
+
     public float xStrength;
+    private float xVelocity;
     public float yStrength;
+
     public float dev_friction;
     private float friction;    
-    private float xVelocity;
+
+
     public bool is_grounded;
     public BoxCollider2D ground_ray;
     public BoxCollider2D left_wall_ray;
     public BoxCollider2D right_wall_ray;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,6 +43,12 @@ public class Character_script : MonoBehaviour
         x_movement();
         y_movement();
 
+        if (sword_equip == 1)
+        {
+            sword1_attack();
+        }
+
+        sword_lifetime -= Time.deltaTime;
 
     }
 
@@ -49,6 +71,7 @@ public class Character_script : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.A))
             {   
+                direction = -1;
                 xVelocity -= xStrength;
             }
         }
@@ -57,6 +80,7 @@ public class Character_script : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.D))
             {
+                direction = 1;
                 xVelocity += xStrength;
             }  
         }  
@@ -76,5 +100,31 @@ public class Character_script : MonoBehaviour
                 yStrength
             );
         }
+    }
+    void sword1_attack()
+    {
+        
+        if (sword_lifetime <= 0f && Input.GetMouseButtonDown(0))
+            {   
+                sword_exists = true;
+                sword_lifetime = sword_lifetime_static;
+
+
+            
+                sword1_instance = Instantiate(
+                    sword1_prefab,
+                    new Vector2(transform.position.x+direction,transform.position.y),
+                    Quaternion.identity, 
+                    this.transform
+                    );
+
+            }
+
+        if (sword_lifetime <= 0f && sword_exists == true)
+        {
+            sword_exists = false;
+            Destroy(sword1_instance.gameObject);
+        }
+
     }
 }
