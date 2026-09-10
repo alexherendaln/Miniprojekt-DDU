a41937e37762e0fdf16049863100536fd3cf7b2c
alexherendaln <alexherendaln@gmail.com>
2026-09-10 12:53:58 +0200

    3 stage attack

--- Assets/Character_script.cs
+++ Assets/Character_script.cs
@@ -6,13 +6,17 @@ public class Character_script : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public BoxCollider2D sword1_prefab;
-    private BoxCollider2D sword1_instance;
+    private BoxCollider2D sword_instance;
+    public BoxCollider2D sword2_prefab;
     private bool sword_exists = false;
-    public float sword_lifetime_static;
+    public float sword1_lifetime_static;
+    public float sword2_lifetime_static;
     private float sword_lifetime = 0f;
-
+    public float dash_strength;
     public float sword_equip = 1;
-
+    private float attack_pattern = 0;
+    public float attack_pattern_buffer;
+    private float attack_pattern_timer;
     private float direction = 1;
 
     public float xStrength;
@@ -21,7 +25,8 @@ public class Character_script : MonoBehaviour
 
     public float dev_friction;
     private float friction;    
-
+    
+    public bool immovable;
 
     public bool is_grounded;
     public BoxCollider2D ground_ray;
@@ -45,7 +50,7 @@ public class Character_script : MonoBehaviour
 
         if (sword_equip == 1)
         {
-            sword1_attack();
+            sword_attack();
         }
 
         sword_lifetime -= Time.deltaTime;
@@ -66,7 +71,8 @@ public class Character_script : MonoBehaviour
     void x_movement()
     {
         xVelocity *= friction;
-
+        if (immovable == false)
+        {
         if (left_wall_ray.IsTouchingLayers(LayerMask.GetMask("Ground")) == false)
         {
             if (Input.GetKey(KeyCode.A))
@@ -83,7 +89,9 @@ public class Character_script : MonoBehaviour
                 direction = 1;
                 xVelocity += xStrength;
             }  
-        }  
+        }     
+        }
+
     }
     void y_movement()
     {
@@ -93,7 +101,7 @@ public class Character_script : MonoBehaviour
         );
         
 
-        if (Input.GetKeyDown(KeyCode.Space) == true && is_grounded)
+        if (Input.GetKeyDown(KeyCode.Space) == true && is_grounded && immovable == false)
         {
             myRigidbody.linearVelocity = new Vector2 (
                 myRigidbody.linearVelocity.x,
@@ -101,30 +109,85 @@ public class Character_script : MonoBehaviour
             );
         }
     }
+
+    void sword_attack()
+    {
+        if (attack_pattern >= 2)
+        {
+            sword2_attack();
+        }
+        else
+        {
+            sword1_attack();
+        }
+
+        if (attack_pattern_timer < 0)
+        {
+            attack_pattern = 0;
+        }
+
+        if (sword_exists == false)
+        {
+            attack_pattern_timer -= Time.deltaTime;
+        }
+
+        if (sword_lifetime <= 0.05 && sword_exists == true)
+        {
+            sword_exists = false;
+            Destroy(sword_instance.gameObject);
+            immovable = false;
+        }
+    }
     void sword1_attack()
     {
         
-        if (sword_lifetime <= 0f && Input.GetMouseButtonDown(0))
+        if (sword_lifetime <= 0 && Input.GetMouseButtonDown(0))
             {   
                 sword_exists = true;
-                sword_lifetime = sword_lifetime_static;
+                sword_lifetime = sword1_lifetime_static;
+                
+                immovable = true;
 
+                attack_pattern_timer = attack_pattern_buffer;
 
-            
-                sword1_instance = Instantiate(
+                xVelocity = dash_strength * direction;
+                myRigidbody.linearVelocity = new Vector2 (
+                xVelocity,
+                0
+                );
+
+                sword_instance = Instantiate(
                     sword1_prefab,
                     new Vector2(transform.position.x+direction,transform.position.y),
                     Quaternion.identity, 
                     this.transform
                     );
-
+                attack_pattern += 1;
             }
+    }
+    void sword2_attack()
+    {
+        
+        if (sword_lifetime <= 0 && Input.GetMouseButtonDown(0))
+            {   
+                sword_exists = true;
+                sword_lifetime = sword2_lifetime_static;
 
-        if (sword_lifetime <= 0f && sword_exists == true)
-        {
-            sword_exists = false;
-            Destroy(sword1_instance.gameObject);
-        }
+                immovable = true;
+                
+                xVelocity = dash_strength*3 * direction;
+                myRigidbody.linearVelocity = new Vector2 (
+                myRigidbody.linearVelocity.x,
+                0
+                );
 
+                sword_instance = Instantiate(
+                    sword2_prefab,
+                    new Vector2(transform.position.x+direction,transform.position.y),
+                    Quaternion.identity, 
+                    this.transform
+                    );
+                attack_pattern = 0;
+            }
     }
 }
