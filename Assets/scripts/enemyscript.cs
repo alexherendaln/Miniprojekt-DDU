563a8d7ad3f5e131de432560b097aed00e2713b6
mahmoodafzaal504-coder <mahmoodafzaal504@gmail.com>
2026-09-17 20:52:48 +0200

    enemy changes
    der er ikke længere behov for game objects for at patrol højre og venstre distancen en enemy bevæger sig kan ændres for det individe enemy

--- Assets/scripts/enemyscript.cs
+++ Assets/scripts/enemyscript.cs
@@ -5,48 +5,66 @@ using UnityEngine;
 
 public class enemyscript : MonoBehaviour
 {
-    public GameObject PointA;
-    public GameObject PointB;
     private Rigidbody2D rb;
-    private Transform currentpoint;
+
     public float Speed;
+    [SerializeField] private float patrolDistance = 3;
+
+    private Vector2 pointA;
+    private Vector2 pointB;
+
+    private bool movingRight = true;
+
     public GameObject Enemy_1;
-    
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        currentpoint = PointB.transform;
+       
+        pointA = new Vector2(transform.position.x - patrolDistance, transform.position.y);
+        pointB = new Vector2(transform.position.x + patrolDistance, transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentpoint == PointB.transform)
+        if (movingRight == true)
         {
             rb.linearVelocity = new Vector2(Speed, 0);
+
+            if (Vector2.Distance(transform.position, pointB) < 0.5f)
+            {
+                movingRight = false;
+            }
         }
         else
         {
             rb.linearVelocity = new Vector2(-Speed, 0);
-        }
 
-        if(Vector2.Distance(transform.position, currentpoint.position) < 0.5f && currentpoint == PointB.transform)
-        {
-            currentpoint = PointA.transform;
-        }
-        if (Vector2.Distance(transform.position, currentpoint.position) < 0.5f && currentpoint == PointA.transform)
-        {
-            currentpoint = PointB.transform;
+            if (Vector2.Distance(transform.position, pointA) < 0.5f)
+            {
+                movingRight = true;
+            }
         }
     }
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
-        Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
-        Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
+        Vector2 leftPoint = new Vector2(
+            transform.position.x - patrolDistance,
+            transform.position.y
+        );
+
+        Vector2 rightPoint = new Vector2(
+            transform.position.x + patrolDistance,
+            transform.position.y
+        );
+
+        Gizmos.DrawWireSphere(leftPoint, 0.5f);
+        Gizmos.DrawWireSphere(rightPoint, 0.5f);
+        Gizmos.DrawLine(leftPoint, rightPoint);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
